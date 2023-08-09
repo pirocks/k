07de92e4a4ddddfa2b302b7856722d320ae83814
Francis Nixon <13nixonf@gmail.com>
2023-08-09 00:49:29 -0400

    Give java less ram

--- k-distribution/src/main/scripts/bin/kserver.bat
+++ k-distribution/src/main/scripts/bin/kserver.bat
@@ -2,6 +2,6 @@
 SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 ECHO Unable to enable extensions
 call "%~dp0\..\lib\setenv.bat"
-SET K_OPTS=-Xms64m -Xmx48G -Xss32m -XX:+TieredCompilation %K_OPTS%
+SET K_OPTS=-Xms64m -Xmx32G -Xss32m -XX:+TieredCompilation %K_OPTS%
 java -Dfile.encoding=UTF-8 -Djava.awt.headless=true -Djansi.force=true %K_OPTS% -ea -cp "%~dp0..\lib\java\*" org.kframework.main.Main -kserver %*
 ::don't call endlocal because that would reset ERRORLEVEL
